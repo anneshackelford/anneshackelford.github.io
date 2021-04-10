82f9661478136f48e1ed43d827b8b8b8f3c32047
anne78460 <anne91620@gmail.com>
2021-04-10 23:47:37 +0200

    project data in markdown

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,33 +1,23 @@
 import React, { useState } from "react";
 import ScrollAnimation from "react-animate-on-scroll";
-// import phoneAndComputer from "../images/PC-and-phone-compressed-moz.jpg";
-import phoneAndComputer from "../images/PC-and-phone-compressed-moz-small 400w 262h.jpg"
+import phoneAndComputer from "../images/PC-and-phone-compressed-moz.jpg";
+// import phoneAndComputer from "../images/PC-and-phone-compressed-moz-small 400w 262h.jpg"
 import projectPortfolio from "../markdown/projectPosts/projectPortfolio.md";
+import projectPortfolioIntro from "../markdown/projectPosts/projectIntroduction.md";
 import ReactMarkdown from "react-markdown";
 import SectionBar from "./SectionBar";
 
 function Project({ featureList }) {
-  const featuresByStatus = {};
-  featuresByStatus["Done"] = [];
-
-  if (featureList) {
-  featureList.forEach((feature) =>
-    featuresByStatus[feature.feature_status].push(feature)
-  );}
-
-  let doneElements = [];
-  featuresByStatus["Done"].forEach((feature, index) => {
-    doneElements.push(
-      <div className="feature" key={index}>
-        <strong>{feature.name}</strong>
-      </div>
-    );
-  });
-
-  const [markdown, setMarkdown] = useState([]);
+  const [markdownProjectPortfolio, setMarkdownProjectPortfolio] = useState([]);
   fetch(projectPortfolio)
     .then((res) => res.text())
-    .then((post) => setMarkdown(post))
+    .then((post) => setMarkdownProjectPortfolio(post))
+    .catch((err) => console.error(err));
+
+  const [markdownProjectIntro, setMarkdownProjectIntro] = useState([]);
+  fetch(projectPortfolioIntro)
+    .then((res) => res.text())
+    .then((post) => setMarkdownProjectIntro(post))
     .catch((err) => console.error(err));
 
   return (
@@ -35,31 +25,11 @@ function Project({ featureList }) {
       <SectionBar id="projects" title="Projects" />
       <div className="featureList">
         <ScrollAnimation animateIn="fadeIn">
-          <span id="featureList"></span>
-          <h1>This Web Developer Portfolio Project</h1>
-          <div className="imageAndIntro">
-            <img
-              className="phoneAndComputer"
-              src={phoneAndComputer}
-              alt="phone and computer photos"
-              width="400px" height="262px"
-            />
-            <div className="details">
-              <p>
-                This website was designed and developed by me as an example of
-                what I can do. It is a responsive full-stack website focusing on
-                the front-end development. You can find information concerning
-                this website project as well as blogs on subjects that I find
-                interesting that are related to information I needed when
-                building this website.
-              </p>
-            </div>
-          </div>
-          <div className="features">
-            <h2>Features </h2>
-            <div className="done">{doneElements}</div>
-            <ReactMarkdown className="portfolio" children={`${markdown}`} />
-          </div>
+          <ReactMarkdown children={`${markdownProjectIntro}`} />
+          <ReactMarkdown
+            className="portfolio"
+            children={`${markdownProjectPortfolio}`}
+          />
         </ScrollAnimation>
       </div>
     </div>
