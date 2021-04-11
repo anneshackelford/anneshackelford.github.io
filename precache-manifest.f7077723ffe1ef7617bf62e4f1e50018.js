c2ba223538172c3408241ceee58e550967616566
anne78460 <anne91620@gmail.com>
2021-04-11 21:47:45 +0200

    Updates

---
+++ precache-manifest.f7077723ffe1ef7617bf62e4f1e50018.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = [
+  {
+    "revision": "c9116aa644cd0af872e5",
+    "url": "/static/css/main.af619d12.chunk.css"
+  },
+  {
+    "revision": "c9116aa644cd0af872e5",
+    "url": "/static/js/main.d72fdb25.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fa446d5d034711f8035a",
+    "url": "/static/js/2.3a05acfa.chunk.js"
+  },
+  {
+    "revision": "5ebfd9ac145e72b79aeac5df92728e0b",
+    "url": "/static/media/aboutCareer.5ebfd9ac.md"
+  },
+  {
+    "revision": "44776652aed10d135b34ee42b22056d6",
+    "url": "/static/media/line-black.44776652.svg"
+  },
+  {
+    "revision": "c9c91d6885a86ee507deec55c48ec489",
+    "url": "/static/media/line-white.c9c91d68.svg"
+  },
+  {
+    "revision": "f86ff49829acccbec3ddbadcb469f5d2",
+    "url": "/static/media/messageOnPhoneDesignDevelop_medium_cropped_compressed_moz.f86ff498.jpg"
+  },
+  {
+    "revision": "70db8c8fe8e21121d6d9ff6863f55b68",
+    "url": "/static/media/projectPortfolio.70db8c8f.md"
+  },
+  {
+    "revision": "4c9b35fa9988743b5edb678e14ba806c",
+    "url": "/static/media/projectIntroduction.4c9b35fa.md"
+  },
+  {
+    "revision": "e7706305bead858ce332782541fcfb99",
+    "url": "/static/media/blogMarkdown.e7706305.md"
+  },
+  {
+    "revision": "4d1338abdbba2677fef48376a42f4133",
+    "url": "/static/media/blogMisc.4d1338ab.md"
+  },
+  {
+    "revision": "45ad20e339c78019c26ebfc278219672",
+    "url": "/static/media/blogResponsiveCSS.45ad20e3.md"
+  },
+  {
+    "revision": "905e95f66c4ddca8336c056e7b13a634",
+    "url": "/static/media/computerScreensAndPhone.905e95f6.jpg"
+  },
+  {
+    "revision": "90e49887bc1ffe329d64088d3a7abd0c",
+    "url": "/static/media/aboutMindMapHeader.90e49887.jpg"
+  },
+  {
+    "revision": "c6d070c558c61321e34c419c0beb6701",
+    "url": "/static/media/whatHaveIDone.c6d070c5.md"
+  },
+  {
+    "revision": "4792b2387712a05d769442f13adc90ff",
+    "url": "/static/media/whatInterestsMe.4792b238.md"
+  },
+  {
+    "revision": "c4b12290d3c90ebc62caa92f1874edfd",
+    "url": "/static/media/whatIsMyEducation.c4b12290.md"
+  },
+  {
+    "revision": "0d7468813c426cdef5f7744c5255e88f",
+    "url": "/static/media/whatIsMyExpertise.0d746881.md"
+  },
+  {
+    "revision": "6d9b1de0f48b07f18e485c000cf7415b",
+    "url": "/static/media/whatIsMyProfessionalProject.6d9b1de0.md"
+  },
+  {
+    "revision": "bfce7f7815e7320217aab52139ba2ffa",
+    "url": "/static/media/whoAmI.bfce7f78.md"
+  },
+  {
+    "revision": "2217e71fb263f844fa396fa28e4fb180",
+    "url": "/static/media/comingFeatures.2217e71f.md"
+  },
+  {
+    "revision": "ad84ed118adc74776b646d0629ec26b1",
+    "url": "/static/media/features_v0_01_0.ad84ed11.md"
+  },
+  {
+    "revision": "5b5c69d9a653d2c05654323c01ab50a6",
+    "url": "/static/media/features_v0_02_0.5b5c69d9.md"
+  },
+  {
+    "revision": "5423de2a63fd80c5ee4a6d2c63d9a26d",
+    "url": "/static/media/features_v0_03_0.5423de2a.md"
+  },
+  {
+    "revision": "d1792a0b48a1c2fd9173a1ecda2a4806",
+    "url": "/static/media/features_v0_04_0.d1792a0b.md"
+  },
+  {
+    "revision": "b7d5c7e8b7bbfe7966fab5aaea037c13",
+    "url": "/static/media/features_v0_05_0.b7d5c7e8.md"
+  },
+  {
+    "revision": "9eccdbc774d83aab0da7ac9ce2613ca3",
+    "url": "/static/media/features_v0_06_0.9eccdbc7.md"
+  },
+  {
+    "revision": "c4e83d8ac7c5ca9812ad674aa8b0ce29",
+    "url": "/static/media/features_v0_07_0.c4e83d8a.md"
+  },
+  {
+    "revision": "54b8e7c02fbf0e04ae657f9a7574c93d",
+    "url": "/static/media/features_v0_08_0.54b8e7c0.md"
+  },
+  {
+    "revision": "8a057dfc431b9ed0385afb25019e6024",
+    "url": "/static/media/features_v0_09_0.8a057dfc.md"
+  },
+  {
+    "revision": "7d083cfb3bb31728c8f1a87a78106d3e",
+    "url": "/static/media/features_v0_10_0.7d083cfb.md"
+  },
+  {
+    "revision": "8a307b5d5e8517bef0d6900f2086c81a",
+    "url": "/static/media/features_v0_11_0.8a307b5d.md"
+  },
+  {
+    "revision": "f0d203f953426da46070a81b5df4bddc",
+    "url": "/static/media/features_v0_12_0.f0d203f9.md"
+  },
+  {
+    "revision": "ca2013510bf5d403d8e9a7eb0f6a82bf",
+    "url": "/static/media/features_v0_13_0.ca201351.md"
+  },
+  {
+    "revision": "3b35daaf857df867085c973178a0a866",
+    "url": "/static/media/features_v0_14_0.3b35daaf.md"
+  },
+  {
+    "revision": "54d88cf7ff89ad4648c1bf7da1ac635b",
+    "url": "/static/media/features_v0_15_0.54d88cf7.md"
+  },
+  {
+    "revision": "52340fd4b807cd4f71120a7b1776640d",
+    "url": "/static/media/features_v0_16_0.52340fd4.md"
+  },
+  {
+    "revision": "63baf70136662523c2bb5368cb4d4ed4",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/testVersion/precache-manifest.67335df94e8c40deb62b5dd8652fd788.js"
+  "/precache-manifest.f7077723ffe1ef7617bf62e4f1e50018.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/testVersion/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
